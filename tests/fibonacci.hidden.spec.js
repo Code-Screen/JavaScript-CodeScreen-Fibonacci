678a0783a109d9ca4f9cf540c4b64120d9ea9c0a
davidcervi <dave@autocruit.io>
2021-02-26 16:38:02 +0400

    Replaced mocha with jest

--- tests/fibonacci.hidden.spec.js
+++ tests/fibonacci.hidden.spec.js
@@ -1,6 +1,4 @@
 import { Fibonacci } from '../fibonacci.js'
-import pkg from 'chai'
-const { expect } = pkg;
 
 const fib = new Fibonacci();
 
@@ -9,19 +7,19 @@ describe("Fibonacci Hidden Test", () => {
     describe("calculate", () => {
 
         it("generates 377", () => {
-            expect(fib.calculate(14)).to.equal(377)
+            expect(fib.calculate(14)).toEqual(377)
         });
 
         it("generates 610", () => {
-            expect(fib.calculate(15)).to.equal(610)
+            expect(fib.calculate(15)).toEqual(610)
         });
 
         it("generates 2584", () => {
-            expect(fib.calculate(18)).to.equal(2584)
+            expect(fib.calculate(18)).toEqual(2584)
         });
 
         it("generates 4181", () => {
-            expect(fib.calculate(19)).to.equal(4181)
+            expect(fib.calculate(19)).toEqual(4181)
         });
 
     });
--- tests/fibonacci.spec.js
+++ tests/fibonacci.spec.js
@@ -1,6 +1,4 @@
 import { Fibonacci } from '../fibonacci.js'
-import pkg from 'chai'
-const { expect } = pkg;
 
 const fib = new Fibonacci();
 
@@ -9,19 +7,19 @@ describe("Fibonacci Test", () => {
     describe("calculate", () => {
 
         it("generates 0", () => {
-            expect(fib.calculate(0)).to.equal(0)
+            expect(fib.calculate(0)).toEqual(0)
         });
 
         it("generates 1", () => {
-            expect(fib.calculate(1)).to.equal(1)
+            expect(fib.calculate(1)).toEqual(1)
         });
 
         it("generates 6765", () => {
-            expect(fib.calculate(20)).to.equal(6765)
+            expect(fib.calculate(20)).toEqual(6765)
         });
 
         it("generates 75025", () => {
-            expect(fib.calculate(25)).to.equal(75025)
+            expect(fib.calculate(25)).toEqual(75025)
         });
 
     });
